19370e36f46a921f418238579fd9398911112c4f
Dany <103103303+danialfach@users.noreply.github.com>
2023-11-22 13:28:46 +0700

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      sortSchema: true,
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
